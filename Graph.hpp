8eb6e0de6cd95a1dde1a792e438fa2a7b0cce58d
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-12-05 21:07:52 -0500

    init

--- Graph.hpp
+++ Graph.hpp
@@ -1,16 +1,34 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
 class Graph
 {
 private:
-
+    const int DEFAULT_SIZE = 1024;
+    int size;
+    unordered_map<int, list<int>> Adj;
 public:
     Graph();
+    ~Graph();
 
     void addEdge(int u, int v);
     void removeEdge(int u, int v);
     bool edgeIn(int u, int v);
+    void deleteVertex(int u);
+    void addVertex(int u);
+
+    unordered_map<int, pair<int, int>> breadthFirstSearch(int s);
+    unordered_map<int, tuple<int, int, int>> depthFirstSearch(bool sort = false);
+    vector<int> getOrdering();
+    void readFromSTDIN();
 };
 
 #include "Graph.cpp"
